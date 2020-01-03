cb2f754b5d40643234a664ce5344c85612f31af2
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2020-01-03 14:10:41 -0800

    Hooked up database to server.

--- build/app.js
+++ build/app.js
@@ -1,10 +1,24 @@
 "use strict";
+require('dotenv').config();
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoClient = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var CONNECTION_URL = process.env.URL;
+var DATABASE_NAME = 'usersDatabase';
 var app = express();
-var bodyparser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+var database, colletion;
 var port = process.env.PORT || 3200;
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
 app.listen(port, function () {
-    console.log("running at port " + port);
+    // console.log(`running at port ${port}`)
+    mongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true }, function (error, client) {
+        if (error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        colletion = database.collection('personnel');
+        console.log("Connected to " + DATABASE_NAME + "!");
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
+const CONNECTION_URL = process.env.URL;
+const DATABASE_NAME = 'usersDatabase';
 
 const app = express();
-const bodyparser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+var database, colletion;
 
 const port = process.env.PORT || 3200;
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
-
 app.listen(port, () => {
-  console.log(`running at port ${port}`)
+  // console.log(`running at port ${port}`)
+  mongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true }, (error: any, client: any) => {
+    if(error) {
+      throw error;
+    }
+    database = client.db(DATABASE_NAME);
+    colletion = database.collection('personnel');
+    console.log(`Connected to ${DATABASE_NAME}!`)
+  });
 });
